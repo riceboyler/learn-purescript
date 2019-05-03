ed821a0e23b06e88cd13ae038c791111c09fec6b
Daniel Gasienica <daniel@gasienica.ch>
2019-05-03 11:26:05 -0400

    Allow selection of exercise to run

--- src/Main.purs
+++ src/Main.purs
@@ -3,9 +3,17 @@ module Main where
 import Prelude
 
 import Effect (Effect)
-import Effect.Console (log, logShow)
+import Effect.Console (log)
+import Node.Process as Process
+import Global as Global
+import Data.Int as Int
 
 main :: Effect Unit
 main = do
-  log "Hello World"
-  logShow { hello: "world" }
+  args <- Process.argv
+  let exercise = case args of
+                    [_, _, ex] ->
+                      Int.floor $ Global.readInt 10 ex
+                    _ ->
+                      0
+  log $ "Run exercise " <> show exercise
