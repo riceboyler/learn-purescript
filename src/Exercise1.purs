b712eb9cc10bc0ca58305d13f1d2c49f4dbbcbb4
Daniel Gasienica <daniel@gasienica.ch>
2019-05-06 13:12:25 -0400

    Use QuickCheck to verify exercise 1

--- src/Exercise1.purs
+++ src/Exercise1.purs
@@ -2,11 +2,80 @@ module Exercise1 where
 
 import Prelude
 
-import Data.Maybe (Maybe(..))
+-- INTERNAL IMPORTS: PLEASE IGNORE
+import Data.Nullable (Nullable, toMaybe, toNullable)
 import Effect (Effect)
-import Effect.Console (logShow)
+import Test.QuickCheck ((===), quickCheck)
+
+-- Imports for exercise:
+import Data.Maybe (Maybe(..))
+
+
+-- TODO: Implement `maybAdd2` to do the same as the JavaScript implementation
+-- in `Exercise1.js` file:
+maybeAdd2 :: Maybe Int -> Maybe Int
+maybeAdd2 _ = Nothing
+
 
 main :: Effect Unit
-main = do
-  logShow $ map (\x -> x + 2) (Just 2)
-  logShow $ map (_ + 2)       (Just 2)
+main = quickCheck \n ->
+  maybeAdd2 n === maybeAdd2JS n
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- INTERNAL: PLEASE IGNORE
+-- JavaScript implementation
+maybeAdd2JS :: Maybe Int -> Maybe Int
+maybeAdd2JS x = toMaybe (maybeAdd2JS_ (toNullable x))
+
+foreign import maybeAdd2JS_ :: Nullable Int -> Nullable Int
