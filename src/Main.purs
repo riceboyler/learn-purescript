ee4e98d0c83d879c9aba74cfb8f21b6433770410
Daniel Gasienica <daniel@gasienica.ch>
2019-05-06 13:50:15 -0400

    fixup! Move internals into `Internal`

--- src/Main.purs
+++ src/Main.purs
@@ -1,18 +1,14 @@
 module Main where
 
-import Prelude
-
-import Effect (Effect)
-import Internal.Runner as Runner
+import Internal
+import Internal (main) as Runner
 
 import Exercises.Exercise1.PureScript as Exercise1
-import Exercise2 as Exercise2
 
 
 allExercises :: Array (Effect Unit)
 allExercises =
   [ Exercise1.main
-  , Exercise2.main
   ]
 
 main :: Effect Unit
